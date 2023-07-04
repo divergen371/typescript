83f4090c18fe24f3937ea77117166ebffc197de4
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-04 20:03:16 +0900

    ねこ画像を取得する

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,14 +1,35 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const IndexPage: NextPage = () => {
-  return <div>ねこ画像予定地</div>;
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  }, []);
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>他のﾈｺﾁｬｰﾝも見てみる</button>
+      <div>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
 };
 
 export default IndexPage;
 
 type Image = {
-    url: string;
-}
+  url: string;
+};
 
 const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");

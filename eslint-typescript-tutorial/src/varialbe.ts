256b919bbb3b9c4758e38d2b5ce242926516b778
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-05 14:27:30 +0900

    let,constは宣言前の変数を参照するとReferenceErrorを発生させる 巻き上げ自体は発生しているが、変数が評価されるまで初期化しないのでReferenceErrorで巻き上げを補足している。

--- eslint-typescript-tutorial/src/varialbe.ts
+++ eslint-typescript-tutorial/src/varialbe.ts
@@ -1,3 +1,11 @@
-console.log(greeting);
-// eslint-disable-next-line vars-on-top, no-var
-var greeting = "hello";
+function output() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-var
+  var x = 1;
+  {
+    // eslint-disable-next-line no-console, @typescript-eslint/no-use-before-define
+    console.log(x);
+    let x = 2;
+  }
+}
+
+output();

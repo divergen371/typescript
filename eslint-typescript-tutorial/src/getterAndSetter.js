89624dca7bffe34f4c3c324b60f077f27df812ce
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-06 16:53:41 +0900

    _valueが外からもアクセスできてしまう

--- eslint-typescript-tutorial/src/getterAndSetter.js
+++ eslint-typescript-tutorial/src/getterAndSetter.js
@@ -1,20 +1,17 @@
 class NumWrapper {
+  _value;
   constructor(value) {
     this._value = value;
   }
 
   get value() {
-    console.log("getter");
     return this._value;
   }
 
   set value(newValue) {
-    console.log("setter");
     this._value = newValue;
   }
 }
 
 const numWrapper = new NumWrapper(1);
-console.log(numWrapper.value);
-numWrapper.value = 42;
-console.log(numWrapper.value);
+console.log(numWrapper._value);

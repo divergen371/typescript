60266934a4aa2b5f25d94255ce5c978b62219049
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-07 14:01:43 +0900

    反復処理keysとvalues キー、値をマップへの挿入順に取得する

--- eslint-typescript-tutorial/src/map_and_set.js
+++ eslint-typescript-tutorial/src/map_and_set.js
@@ -2,8 +2,22 @@ const map = new Map([
   ["a", 1],
   ["b", 2],
 ]);
-const results = [];
-map.forEach((value, key) => {
-  results.push(`${key}:${value}`);
-});
-console.log(results);
+
+const keys = [];
+
+for (const key of map.keys()) {
+  keys.push(key);
+}
+
+console.log(keys);
+const keysArray = Array.from(map.keys());
+console.log(keysArray);
+
+const values = [];
+for (const value of map.values()) {
+  values.push(value);
+}
+
+console.log(values);
+const valueArray = Array.from(map.values());
+console.log(valueArray);

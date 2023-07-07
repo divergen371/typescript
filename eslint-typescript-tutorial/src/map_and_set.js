5051e3947d6e0bdaf1e5d31d438ccfd2229f4593
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-07 13:48:51 +0900

    反復処理forEach

--- eslint-typescript-tutorial/src/map_and_set.js
+++ eslint-typescript-tutorial/src/map_and_set.js
@@ -1,20 +1,9 @@
 const map = new Map([
   ["a", 1],
-  ["b", 1],
+  ["b", 2],
 ]);
-console.log(map.size);
-map.set("c", 3);
-console.log(map.size);
-console.log(map.get("a"));
-map.set("a", 5);
-console.log(map.get("a"));
-//キーの存在確認
-console.log(map.has("a"));
-console.log(map.has("e"));
-
-// 要素の削除
-map.delete("a");
-console.log(map.has("a"));
-// すべて削除
-map.clear();
-console.log(map.size);
+const results = [];
+map.forEach((value, key) => {
+  results.push(`${key}:${value}`);
+});
+console.log(results);

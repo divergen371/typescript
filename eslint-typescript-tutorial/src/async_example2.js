c99926527799b3ac757431ae3ea397de205725ed
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-07 10:43:15 +0900

    非同期処理もメインスレッドで実行されているので同期処理のブロックの影響を受ける

--- eslint-typescript-tutorial/src/async_example2.js
+++ eslint-typescript-tutorial/src/async_example2.js
@@ -9,9 +9,26 @@ function taskB() {
 function taskAsync() {
   console.log("非同期タスクを実行 at " + Date(Date.now()));
 }
+function blockTime(timeout) {
+  const startTime = Date.now();
+  while (true) {
+    const diffTime = Date.now() - startTime;
+    if (diffTime >= timeout) {
+      return;
+    }
+  }
+}
 
+const startTime = Date.now();
 taskA();
 setTimeout(() => {
+  const endTime = Date.now();
   taskAsync();
-}, 1000);
+  console.log(
+    `非同期処理のコールバックが呼ばれるまで${
+      endTime - startTime
+    }ミリ秒かかりました`
+  );
+}, 10);
+blockTime(1000);
 taskB();

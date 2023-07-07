2f243d1fe52366fc145a9a7bed75b940f34c2e4b
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-08 00:08:26 +0900

    catchで拾うだけのシンプルなエラーハンドリング

--- js-primer/cli/main.js
+++ js-primer/cli/main.js
@@ -4,6 +4,11 @@ import * as fs from "node:fs/promises";
 program.parse(process.argv);
 const filePath = program.args[0];
 
-fs.readFile(filePath, {encoding: "utf8"}).then((file) => {
-  console.log(file);
-});
+fs.readFile(filePath, { encoding: "utf8" })
+  .then((file) => {
+    console.log(file);
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });

2b85a1759973a1731d0d08150ef18e56db1331a5
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-08 00:17:55 +0900

    オプションを定義する

--- js-primer/cli/main.js
+++ js-primer/cli/main.js
@@ -2,12 +2,20 @@ import { program } from "commander";
 import { marked } from "marked";
 import * as fs from "node:fs/promises";
 
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
+const opts = program.opts();
+const cliOpts = {
+  gfm: opts.gfm ?? false,
+};
+
 fs.readFile(filePath, { encoding: "utf8" })
   .then((file) => {
-    const html = marked.parse(file);
+    const html = marked.parse(file, {
+      gfm: cliOpts.gfm,
+    });
     console.log(html);
   })
   .catch((err) => {

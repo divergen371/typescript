5a2a015641d42447af7e27b7be897dae91fa76e8
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-07 15:38:49 +0900

    ボタンの押下をAPIからのデータ取得のトリガーにする

--- js-primer/ajaxapp/index.js
+++ js-primer/ajaxapp/index.js
@@ -1,16 +1,17 @@
-const userId = "divergen371";
 
-fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-  .then((res) => {
-    console.log(res.status);
-    if (!res.ok) {
-      console.log("エラーレスポンス", res);
-    } else {
-      return res.json().then((userInfo) => {
-        console.log(userInfo);
-      });
-    }
-  })
-  .catch((err) => {
-    console.log(error);
-  });
+function fetchUserInfo(userId) {
+  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((res) => {
+      console.log(res.status);
+      if (!res.ok) {
+        console.log("エラーレスポンス", res);
+      } else {
+        return res.json().then((userInfo) => {
+          console.log(userInfo);
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(error);
+    });
+}

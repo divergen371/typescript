f883cf178432a5b6e2233b86b63fd03f334b90e7
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-07 10:07:51 +0900

    ビルトインエラーを投げる

--- eslint-typescript-tutorial/src/exception.js
+++ eslint-typescript-tutorial/src/exception.js
@@ -1,7 +1,14 @@
+function reverseString(str) {
+  if (typeof str !== "string") {
+    throw new TypeError(`${str} is not a string`);
+  }
+  return Array.from(str).reverse().join("");
+}
+
 try {
-  eval("foo bar");
+  reverseString(100);
 } catch (error) {
-  console.log(error instanceof SyntaxError);
+  console.log(error instanceof TypeError);
   console.log(error.name);
   console.log(error.message);
 }

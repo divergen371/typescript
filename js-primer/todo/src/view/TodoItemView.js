c42ed8ec963b79c98c3ea6331bbef00c5dc0926f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-08 21:21:28 +0900

    Todoアプリ

---
+++ js-primer/todo/src/view/TodoItemView.js
@@ -0,0 +1,22 @@
+import { element } from "./html-util.js";
+export class TodoItemView {
+  createElement(todoItem, { onUpdateTodo, onDeleteTodo }) {
+    const todoItemElement = todoItem.completed
+      ? element`<li><input type="checkbox" class="checkbox" checked><s>${todoItem.title}</s><button class="delete">x</button></li>`
+      : element`<li><input type="checkbox" class="checkbox">${todoItem.title}<button class="delete">x</button></li>`;
+    const inputCheckBoxElement = todoItemElement.querySelector(".checkbox");
+    inputCheckBoxElement.addEventListener("change", () => {
+      onUpdateTodo({
+        id: todoItem.id,
+        completed: !todoItem.completed,
+      });
+    });
+    const deleteButtonElement = todoItemElement.querySelector(".delete");
+    deleteButtonElement.addEventListener("click", () => {
+      onDeleteTodo({
+        id: todoItem.id,
+      });
+    });
+    return todoItemElement;
+  }
+}
